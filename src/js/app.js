90dafe1e7e37e2d50c213d09a13d89ac7c5f2659
Anton <elizarovanton35@gmail.com>
2023-02-05 16:48:54 +0200

    f

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ import * as ibg from './modules/ibg.js';
 import * as anchors from './modules/anchors.js';
 import * as useDynamicAdapt from './modules/dynamicAdapt.js';
 
+// import jBox from 'jbox';
+
 isWebp.isWebp(); // Проверка поддержки webp и добавление класса _webp или _no-webp для HTML
 ibg.ibg(); // Фоновое изображение
 anchors.anchors(); // Плавный скролл
@@ -45,4 +47,41 @@ links.forEach(function (i) {
     menuBtn.classList.remove('active');
     overlay.classList.remove('active');
   });
+});
+
+
+const allModals = document.querySelectorAll('[data-modal]');
+const modalButtons = document.querySelectorAll('[data-modal-button]');
+const modalCloseBtn = document.querySelectorAll('[data-modal-close]');
+
+modalButtons.forEach(function (i) {
+  i.addEventListener("click", function (e) {
+    e.preventDefault();
+    body.classList.add('lock')
+    const modalId = this.dataset.modalButton;
+    const modal = document.querySelector('#' + modalId);
+    modal.classList.remove('visibility-hidden');
+    overlay.classList.add('active');
+    const modalWindow = modal.querySelector('.modal__window');
+    modalWindow.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+  });
+});
+
+modalCloseBtn.forEach(function (i) {
+  i.addEventListener("click", function (e) {
+    body.classList.remove('lock')
+    e.preventDefault();
+    const modal = this.closest('[data-modal]');
+    modal.classList.add('visibility-hidden');
+    overlay.classList.remove('active');
+  });
+});
+
+overlay.addEventListener("click", function () {
+  body.classList.remove('lock')
+  allModals.forEach(function (i) {
+    i.classList.add('visibility-hidden');
+  })
 });
